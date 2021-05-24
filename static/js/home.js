4183f29d4c9dc5a61ca397d676769b4192ad3ec7
ManasUniyal <manasuniyal100@gmail.com>
2021-05-24 18:50:04 +0530

    Added js for home page

---
+++ static/js/home.js
@@ -0,0 +1,28 @@
+//Handling image upload button
+const img_upload_btn = document.getElementById("img_upload")
+const upload_img = document.getElementById("image")
+const predict_btn = document.getElementById("predict_btn")
+const form = document.getElementById("submit_image_form")
+
+//Handling image upload button
+img_upload_btn.addEventListener("change", function () {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.addEventListener("load", function () {
+            upload_img.setAttribute("src", String(this.result));
+        });
+        reader.readAsDataURL(file);
+        predict_btn.style.display = "block";
+    } else {
+        upload_img.src = "/static/images/default_image.gif";
+        predict_btn.style.display = "none";
+    }
+})
+
+//Handling prediction button
+if (predict_btn) {
+    predict_btn.onclick = function () {
+        form.submit();
+    }
+}
