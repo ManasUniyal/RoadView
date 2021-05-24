7c0d7b84babdbab33f560117add1202863f865f7
ManasUniyal <manasuniyal100@gmail.com>
2021-05-25 03:01:22 +0530

    Added and integrated ML model

--- cnn_model/compute_result.py
+++ cnn_model/compute_result.py
@@ -1,2 +1,53 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import os
+from tensorflow.keras.preprocessing.image import save_img
+
+
+def resize_img(path, shape = 128):
+    img = plt.imread(path)
+    resized_img = cv2.resize(img, (shape, shape))
+    return resized_img
+
+def convert_to_tensor(img):
+    resized_img = img[None, ...]
+    return resized_img
+
+def predict(tensor, model):
+    output = model.predict(tensor)
+    return output
+
+def squeeze_img(img):
+    img = np.squeeze(img, 0)
+    img = np.squeeze(img, 2)
+    return img
+
+def get_result(img_path, model):
+    img = resize_img(img_path)
+    tensor = convert_to_tensor(img)
+    output = predict(tensor, model)
+    result = squeeze_img(output)
+    return result
+
+def save_result(write_path, img):
+    final_result = tf.keras.preprocessing.image.img_to_array(img)
+    save_img(write_path, final_result)
+
 def process_image(upload_directory, result_directory, file_name):
-    pass
+
+    unet_model = tf.keras.models.load_model('cnn_model/route_generator_weights.h5')
+
+    read_path = upload_directory + file_name
+    print(read_path)
+
+    result = get_result(read_path, unet_model)
+
+    if not os.path.exists(result_directory):
+        os.mkdir(result_directory)
+
+    write_path = result_directory + file_name
+
+    save_result(write_path, result)
+
--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render, redirect
@@ -15,27 +17,29 @@ def result(request):
 
     if request.method == 'POST':
 
-        upload_directory = settings.MEDIA_ROOT / str(request.user.id) / "/uploads/"
-        result_directory = settings.MEDIA_ROOT / str(request.user.id) / "/results/"
-        upload_file_url = None
+        upload_directory = str(settings.MEDIA_ROOT) + "/" + str(request.user.id) + "/uploads/"
+        print(upload_directory)
+        result_directory = str(settings.MEDIA_ROOT) + "/" + str(request.user.id) + "/results/"
+        uploaded_file_url = None
         processed_file_url = None
 
         if request.FILES.get('image'):
             uploaded_image = request.FILES.get('image')
             fs = FileSystemStorage(location=upload_directory)  # defaults to MEDIA_ROOT
             file_name = fs.save(uploaded_image.name, uploaded_image)
-            upload_file_url = '/images/' + str(request.user.id) + "/uploads/" + file_name
+            uploaded_file_url = '/images/' + str(request.user.id) + "/uploads/" + file_name
             compute_result.process_image(upload_directory, result_directory, file_name)
             processed_file_url = '/images/' + str(request.user.id) + "/results/" + file_name
 
         if request.user.is_authenticated:
             RoadExtraction.objects.create(
                 user=request.user,
-                uploaded_image_url=upload_file_url,
+                uploaded_image_url=uploaded_file_url,
                 processed_image_url=processed_file_url,
             )
 
         context = {
+            'uploaded_image_url': uploaded_file_url,
             'processed_image_url': processed_file_url
         }
 
