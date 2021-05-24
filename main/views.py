0e6facfc379858b0f08a1effa3a6d169da072db6
ManasUniyal <manasuniyal100@gmail.com>
2021-05-24 19:19:01 +0530

    Added result page

--- main/views.py
+++ main/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def home(request):
-    return render(request, 'main/homepage.html')
+    if request.method == 'GET':
+        return render(request, 'main/homepage.html')
+
+
+def result(request):
+    if request.method == 'POST':
+        return render(request, 'main/result.html')
